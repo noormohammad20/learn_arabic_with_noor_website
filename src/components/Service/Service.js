bf2fb1829b48bdbc331b7c553e9bfb2515f9c886
noormohammad20 <mdsaim204@gmail.com>
2022-04-18 17:04:39 +0600

    some style chenge on services page

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -9,7 +9,7 @@ const Service = ({ service }) => {
     }
     return (
         <div className='service me-5 card-body d-flex  flex-column' >
-            <img className='w-100 rounded-pill' src={img} alt="" />
+            <img className='img w-100' src={img} alt="" />
             <h2 className='service-name text-success'>{name}</h2>
             <h5 className='service-price'>{price}</h5>
             <p>{description}</p>
